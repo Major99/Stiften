8ec1bf59694bd66f56a95939f8acafb9597a210b
Aman Sharma <amandecembersharma@gmail.com>
2021-09-25 06:53:37 +0530

    two section of main page completed

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- client/pages/index.js
+++ client/pages/index.js
@@ -4,11 +4,25 @@ import Link from 'next/link'
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
-import { Button } from 'antd';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-
+import { Button, Divider, Card } from 'antd';
 
 const Home = ()=>{
+
+  const icon=(imgname,header,para)=>(
+    <div className={styles.iconsec}>
+          <>
+          <img src={imgname}
+          alt='donation pic 1'
+          className={styles.icons2}/>
+          </>
+          <div className={styles.iconContent}>
+            <h3 className={styles.section2header}>{header}</h3>
+            <p>{para}</p>
+          </div>
+    </div>
+  )
+  const { Meta } = Card;
 
   return(
     <>
@@ -22,7 +36,6 @@ const Home = ()=>{
           <h1 className={styles.contentheader}>Charity Is An Act Of A Soft Heart.</h1>
           <p className={styles.contentpara}>Cupidatate aute doloree Lorem ex aliqua. Ipsum aute voluptate pariatur laboris sit Lorem commodo adipisicing sunt veniam est. Pariatur labore quis quis aute reprehenderit reprehenderit adipisicing dolore deserunt nulla reprehenderit.</p>
           <button className={styles.buttonMember}>Become A Member Now <ArrowForwardIcon/></button>
-          
         </div>
         <div 
           className={styles.pic1}>
@@ -34,12 +47,67 @@ const Home = ()=>{
       </section>
 
       <section className={styles.section2}>
-        <h1>section2</h1>
-        
+        {icon('/donation(1).png','985+','Donations Received')}
+        {icon('/donation.png','₹10 L','Money Donated')}
+        {icon('/campaign.png','12+','Active Campaign')}
+        {icon('/heart.png','₹60 L','Charity in last year')}
       </section>
 
       <section className={styles.section3}>
-        <h1>section3</h1>
+        <div className={styles.campaignSec}>
+          <div className={styles.campaignsecleft}>
+            <h1>Support Your Community</h1>
+            <p>Officia sint adipidsicing officia consectetur aliquip cupidatat amet voluptate irure ullamco velit ut. Exercitation laborum occaecat exercitation sint nulla dolor dolore in nostrud. Aute tempor duis Lorem reprehenderit commodo id nostrud enim ullamco fugiat velit adipisicing quis. Adipisicing esse culpa officia esse nostrud commodo eiusmod ipsum occaecat nulla. Esse quis sunt esse incididunt cupidatat culpa non.</p>
+            <div className={styles.cards}>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              >
+                <Meta title="Covid-19" />
+              </Card>
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+              >
+                <Meta title="Dog Food" />
+              </Card>
+            </div>
+          </div>
+          <div className={styles.campaignsecright}>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+              <Meta title="Food Bank" />
+            </Card>
+            
+            <Link href='/'><a className={styles.allcamplink}>View All Campaigns</a></Link>
+          </div>
+        </div>
+
+        <hr/>
+        <div className={styles.about}>
+          <h1>About Us</h1>
+        </div>
+
+      <hr/>
+        <div className={styles.teamSec}>
+      <h1>TeamSec</h1>
+        </div>
+
+      <hr/>
+        <div className={styles.testimonialSec}>
+      <h1>testimoial sec</h1>
+        </div>
+      </section>
+
+      <section className={styles.section4}>
+        <h1>
+          section4
+        </h1>
       </section>
     </Layout>
     </>
